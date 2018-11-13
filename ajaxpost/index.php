7420cf69ef9ba02b4172199a974b7e1df3e96e83
Heru Prasetyo <heruprasetyo96@gmail.com>
2018-11-14 01:19:28 +0700

    Create index.php

---
+++ ajaxpost/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+
+
+</head>
+<body>
+    <form action="laporan.php" method="POST" class="ajax">
+    <div><input type="text" name="name" placeholder="Name"></div>
+    <div><input type="text" name="email" placeholder="email"></div>
+    <div><textarea name="message" placeholder="message"></textarea></div>
+    <input type="submit" value="send">
+    </form>   
+    
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+    <script src="main.js"></script>
+</body>
+</html>
